a9e603a8beabe0cd1361ac627d72019edaa163a8
Sui Zer <teysuizer1998@gmail.com>
2024-06-27 20:54:58 +0800

    Reformat gallery to Carousell

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,6 +1,6 @@
 import { styled } from '@stitches/react'
-import { Col, Image, Row } from 'antd'
-import { useRef, useState } from 'react'
+import { Carousel, Image } from 'antd'
+import { useRef } from 'react'
 import { useWindowSize } from 'react-use'
 
 import { ConfigsType } from '../configs'
@@ -10,14 +10,16 @@ const isPortrait = window.matchMedia('(orientation: portrait)').matches
 
 const Layout = styled('div', {
   width: '100%',
-  padding: isPortrait ? '20% 0% 15% 5%' : '5% 0% 5% 10%'
+  padding: isPortrait ? '10% 0% 10% 0%' : '2.5% 0% 2.5% 0%',
+  alignItems: 'center',
+  justifyContent: 'center',
+  textAlign: 'center'
 })
 
 const Title = styled('p', {
   color: '#FFFFFF',
   width: '100%',
-  fontSize: isPortrait ? '2.5em' : '3.5em',
-  margin: 0,
+  fontSize: isPortrait ? '2em' : '2.5em',
   fontWeight: '500',
   fontFamily: 'Great Vibes'
 })
@@ -32,38 +34,33 @@ const Gallery = ({ config }: GalleryProps) => {
   const ref = useRef<HTMLSelectElement>(null)
   const onScreen: boolean = useOnScreen<HTMLDivElement>(ref, '-125px')
 
-  const [previewVisible, setPreviewVisible] = useState<boolean>(false)
-  const [previewIndex] = useState<number>(0)
-
   return (
     <section
       ref={ref}
       style={{
         height: '100vh',
         background: onScreen ? '#212121' : '#EFEBE9',
-        overflow: 'hidden',
-        position: 'relative',
         transition: 'background 1s ease-in'
       }}
     >
       <Layout>
-        <Title>Our Beautiful Moments...</Title>
-      </Layout>
-      <Row gutter={[16, 16]}>
-        <Image.PreviewGroup
-          preview={{
-            visible: previewVisible,
-            onVisibleChange: (visible) => setPreviewVisible(visible),
-            current: previewIndex
-          }}
+        <Title>Our Precious Moments...</Title>
+        <Carousel
+          autoplay={true}
+          effect="fade"
+          arrows={true}
+          draggable={true}
+          slidesToShow={1}
+          centerMode={true}
+          speed={300}
         >
           {config.galleryImages.map((image, index) => (
-            <Col key={index} span={isPortrait ? 6 : 3}>
-              <Image key={index} src={image} width={isPortrait ? width / 4 - 10 : width / 8 - 10} />
-            </Col>
+            <div key={index} onClick={() => {}}>
+              <Image src={image} width={isPortrait ? width * 0.5 : width * 0.2} preview={true} />
+            </div>
           ))}
-        </Image.PreviewGroup>
-      </Row>
+        </Carousel>
+      </Layout>
     </section>
   )
 }
