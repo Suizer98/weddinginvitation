71d7bece5b416fabf848e2c7049f82d2849515bc
Suizer98 <93068036+Suizer98@users.noreply.github.com>
2025-01-24 22:15:47 +0800

    Hide webpage for now

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,12 @@ import 'antd/dist/antd.min.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import App from './App'
+// import App from './App'
 import './index.css'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+root.render(<React.StrictMode>{/* <App /> */}</React.StrictMode>)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
